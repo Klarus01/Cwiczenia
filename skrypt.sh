1b50ff7b9e990b71d15c8d702f7ea49256cd1708
Klarus01 <44645536+Klarus01@users.noreply.github.com>
2022-07-01 19:39:16 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -3,4 +3,10 @@ option=$1
 if [ $option == "--date" ]
 then
         echo $(date)
+elif [ $option == "--logs" ]
+then
+        for (( i = 1; i<=100; i++ ))
+        do
+                echo log$i, $(date), $0 >> log$i.txt
+        done
 fi
