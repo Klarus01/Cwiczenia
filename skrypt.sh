65a378c06b1311bdff9550a3cfd416ea858f3407
Klarus01 <44645536+Klarus01@users.noreply.github.com>
2022-07-01 20:01:52 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -12,11 +12,13 @@ then
         fi
         for (( i = 1; i<=$value; i++ ))
         do
-                echo log$i, $(date), $0 >> log$i.txt
+                $(mkdir log$i)
+                touch ./log$i/log$i.txt
+                echo log$i, $(date), $0 >> ./log$i/log$i.txt
         done
 elif [ $option == "--help" ] || [ $option == "-h" ]
 then
         echo "--date or -d - wyswietla aktualna date"
         echo "--logs x or -l x - tworzy x plikow log, podstawowa wartos - 100"
-        echo "--help or -h - wyswietla wszystkie komendy"
+        echo "--help or -d - wyswietla wszystkie komendy"
 fi
